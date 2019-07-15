79342f18c25b65381a4a4116e887341c59089848
hamke <the235style@yahoo.com>
2019-07-16 08:12:26 +0900

    Site updated: 2019-07-16 08:12:25

--- js/main.js
+++ js/main.js
@@ -1,40 +1,10 @@
 $(document).ready(function(){
-      $(document).bind('contextmenu', function(e) {
-        if(!$(e.target).parents().hasClass('allowContent')) {
-          alert('오른쪽 마우스 클릭은 사용할 수 없습니다. 서비스를 이용해 주셔서 감사합니다.');
-          return false;
-        } else {
-          return true;
-        }
-      });
-      $(document).bind('selectstart', function(e) {
-          return $(e.target).parents().hasClass('allowContent');
-      });
-      $(document).bind('dragstart', function(e) {
-          return $(e.target).parents().hasClass('allowContent');
-      });
-      $(document).bind('copy', function(e) {
-          return $(e.target).parents().hasClass('allowContent');
-      });
-      $(document).bind('cut', function(e) {
-          return $(e.target).parents().hasClass('allowContent');
-      });
-      $(document).bind('paste', function(e) {
-          return $(e.target).parents().hasClass('allowContent');
-      });
-      $(function(){
-        $('#allowContent').addClass('allowContent');
-      });
-  });
-  function mouseon(){
-    document.body.classList.add('allowContent');
-  }
-
-$(document).ready(function(){
     $(window).scroll(function () {
        if ($(this).scrollTop() > 50) {
+         $('#chat-button').fadeIn();
          $('#back-to-top').fadeIn();
        } else {
+         $('#chat-button').fadeOut();
          $('#back-to-top').fadeOut();
        }
      });
